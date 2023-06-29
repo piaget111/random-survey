6e82ad8e8df06be58ac19e425ed621247955e7f9
piaget111 <126682287+piaget111@users.noreply.github.com>
2023-06-29 21:49:03 +0800

    Update url.js

--- url.js
+++ url.js
@@ -4,10 +4,10 @@
  * 並請注意：
  * 1. 除最後一項外，其餘項目用逗號分隔
  * 2. 網址請用引號包起來
+ 'https://www.surveycake.com/s/8A3e1',
  */
 
 
 const urls = [
-	'https://www.surveycake.com/s/8A3e1',
 	'https://www.surveycake.com/s/AP6PO'
 ];
